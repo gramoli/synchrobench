7fc29949ad9f678cf16a5e706af33d6b74424810
Vitaly Aksenov <vaksenov@sphinx.zoo>
2016-09-23 18:37:06 +0300

    all scripts

---
+++ java/scripts/process.py
@@ -0,0 +1,55 @@
+import sys
+import os
+import re
+from statistics import mean
+
+def read_from_file(filename, keys):
+    inf = open(filename, 'r')
+    values = dict()
+    for key in keys:
+        values[key] = []
+    for line in inf.readlines():
+        ll = line.lower()
+        good = None
+        for key in keys:
+            if key.lower() in ll:
+                good = key
+        if good == None:
+            continue
+        value = re.sub('(.*?)', '', ll).strip().split(" ")[-1]
+        values[key].append(float(value))
+    return values
+
+def filename(bench, size, write, proc):
+   return "../output/log/" + bench + "-i" + str(size) + "-u" + str(write) + "-t" + str(proc) + ".log"
+
+keys = ["throughput"]
+procs = [1, 2, 4, 8, 16]
+sizes = [16384, 65536]
+writes = [0, 20, 40, 60, 80, 100]
+benchmarks = ["trees.lockbased.ConcurrencyOptimalBSTv2",
+              "trees.lockbased.LockBasedFriendlyTreeMap",
+              "trees.lockbased.LockBasedStanfordTreeMap",
+              "trees.lockfree.NonBlockingTorontoBSTMap"]
+if not os.path.isdir:
+    os.mkdir("../output/data")
+for write in writes:
+    out = open("../output/data/trees_comparison_" + str(write) + ".txt", 'w')
+    for key in keys:
+        out.write("Key " + key + "\n")
+        for size in sizes:
+            out.write("Size " + str(size) + "\n")
+            buffer = "{:^45}".format("benchmark name")
+            for proc in procs:
+                buffer += "{:^25}".format(str(proc))
+            out.write(buffer + "\n")
+            for bench in benchmarks:
+                buffer = "{:^45}".format(bench)
+                thr1 = mean(read_from_file(filename(bench, size, write, 1), keys)[key])
+                for proc in procs:
+                     thr = mean(read_from_file(filename(bench, size, write, proc), keys)[key])
+                     buffer += "{:^25}".format("{0:.3f}".format(thr) + " ({0:.3f})".format(thr / thr1))
+                out.write(buffer + "\n")
+            out.write("\n")
+    out.close()
+
---
+++ java/scripts/trees.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+dir=..
+output=${dir}/output
+deuce="${dir}/lib/mydeuce.jar"
+agent=${dir}/lib/deuceAgent-1.3.0.jar
+bin=${bin}/bin
+java=java
+javaopt=-server
+
+threads="1 2 4 8 16 24"
+sizes="16384 65536"
+
+writes="0 20 40 60 80 100"
+duration="5000"
+warmup="0"
+snapshot="0"
+writeall="0"
+iterations="5"
+
+JVMARGS=-Dorg.deuce.exclude="java.util.*,java.lang.*,sun.*"
+BOOTARGS=-Xbootclasspath/p:${dir}/lib/rt_instrumented.jar
+CP=${dir}/lib/compositional-deucestm-0.1.jar:${dir}/lib/mydeuce.jar:${dir}/bin
+MAINCLASS=contention.benchmark.Test
+
+if [ ! -d "${output}" ]; then
+  mkdir $output
+fi
+if [ -d "${output}/log" ]; then
+  rm -rf ${output}/log
+fi
+
+mkdir ${output}/log
+
+benchs="trees.lockbased.ConcurrencyOptimalBST trees.lockbased.ConcurrencyOptimalBSTv2 trees.lockbased.LockBasedFriendlyTreeMap trees.lockbased.LockBasedStanfordTreeMap trees.lockfree.NonBlockingTorontoBSTMap"
+for bench in ${benchs}; do
+  for write in ${writes}; do
+    for t in ${threads}; do
+       for i in ${sizes}; do
+         r=`echo  "2*${i}" | bc`
+         out=${output}/log/${bench}-i${i}-u${write}-t${t}.log
+         for (( j=1; j<=${iterations}; j++ )); do
+           echo "${java} ${javaopt} -cp ${CP} ${MAINCLASS} -W ${warmup} -u ${write} -a ${writeall} -s ${snapshot} -d ${duration} -t ${t} -i ${i} -r ${r} -b ${bench}"
+           ${java} ${javaopt} -cp ${CP} ${MAINCLASS} -W ${warmup} -u ${write} -d ${duration} -t ${t} -i ${i} -r ${r} -b ${bench} 2>&1 >> ${out}
+         done
+       done
+    done
+  done
+done
