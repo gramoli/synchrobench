52a77eda20bd9332b655c06b3825971a167409ff
Harrison Rodgers <harrison@harrisonrodgers.com>
2016-10-03 15:32:15 +1100

    Added Versioned List in c-cpp

---
+++ c-cpp/src/linkedlists/versioned/intset.h
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdatomic.h>
+#include <pthread.h>
+
+typedef int val_t;
+#define VAL_MIN INT_MIN
+#define VAL_MAX INT_MAX
+
+// These structs should be defined in <algo>.h
+typedef struct node node_t;
+typedef struct intset intset_t;
+
+// These live in mixin.c, or <algo>.c if special initialisation/destruction is required.
+intset_t *set_new(void);
+void set_delete(intset_t *set);
+int set_size(intset_t *set);
+node_t *new_node(val_t val, node_t *next);
+void set_print(intset_t *set);
+
+// These live in <algo>.c
+int set_contains(intset_t *set, val_t val);
+int set_insert(intset_t *set, val_t val);
+int set_remove(intset_t *set, val_t val);
+
+// Locked operations
+#ifdef MUTEX
+typedef pthread_mutex_t ptlock_t;
+#  define INIT_LOCK(lock)               pthread_mutex_init((pthread_mutex_t *) lock, NULL);
+#  define DESTROY_LOCK(lock)            pthread_mutex_destroy((pthread_mutex_t *) lock)
+#  define LOCK(lock)                    pthread_mutex_lock((pthread_mutex_t *) lock)
+#  define UNLOCK(lock)                  pthread_mutex_unlock((pthread_mutex_t *) lock)
+#else
+typedef pthread_spinlock_t ptlock_t;
+#  define INIT_LOCK(lock)               pthread_spin_init((pthread_spinlock_t *) lock, PTHREAD_PROCESS_PRIVATE);
+#  define DESTROY_LOCK(lock)            pthread_spin_destroy((pthread_spinlock_t *) lock)
+#  define LOCK(lock)                    pthread_spin_lock((pthread_spinlock_t *) lock)
+#  define UNLOCK(lock)                  pthread_spin_unlock((pthread_spinlock_t *) lock)
+#endif
---
+++ c-cpp/src/linkedlists/versioned/mixin.c
@@ -0,0 +1,65 @@
+/* To be included into <algo>.c if all that is needed is:
+ * - Init a set with head and tail node VAL_MIN and VAL_MAX resp.
+ * - New nodes are calloced.
+ */
+
+// Create a new set.
+intset_t *set_new(void) {
+  intset_t *set = malloc(sizeof(intset_t));
+  if (NULL == set) {
+    perror("malloc");
+    exit(1);
+  }
+
+  node_t *max = new_node(VAL_MAX, NULL);
+  node_t *min = new_node(VAL_MIN, max);
+  set->head = min;
+  return set;
+}
+
+// Delete the set and all its elements.
+void set_delete(intset_t *set) {
+  node_t *prev, *curr;
+  curr = set->head;
+  while (NULL != curr) {
+    prev = curr;
+    curr = curr->next;
+    free(prev);
+  }
+  free(set);
+}
+
+// Get the size of the set, excluding the two head and
+// tail sentinels.
+int set_size(intset_t *set) {
+  int size = 0;
+  node_t *curr = set->head->next;
+  while (curr->next != NULL) {
+    size++;
+    curr = curr->next;
+  }
+  return size;
+}
+
+node_t *new_node(val_t val, node_t *next) {
+  node_t *node = calloc(sizeof(node_t), 1);
+  if (NULL == node) {
+    perror("malloc");
+    exit(1);
+  }
+  node->val = val;
+  node->next = next;
+  return node;
+}
+
+// For debugging
+void set_print(intset_t *set) {
+  node_t *curr = set->head;
+  while (curr != NULL) {
+    printf("%d", curr->val);
+    if (curr->next != NULL)
+      printf(" -> ");
+    curr = curr->next;
+  }
+  printf("\n");
+}
