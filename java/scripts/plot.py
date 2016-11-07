a247417148e5845642c5ab2600154e9ecf768979
vaksenov <github12345>
2016-11-07 13:05:33 +0100

    updated script

--- java/scripts/plot.py
+++ java/scripts/plot.py
@@ -24,20 +24,21 @@ def filename(bench, size, write, proc, warmup, duration):
    return "../output/log/" + bench + "-i" + str(size) + "-u" + str(write) + "-t" + str(proc) + "-w" + str(warmup) + "-d" + str(duration) + ".log"
 
 keys = ["throughput"]
-procs = [1, 2, 4, 8, 16, 24, 32]
+procs = [1, 2, 4, 8, 16, 24, 32, 39, 47, 55, 63, 71, 79]
 sizes = [16384, 65536, 262144, 524288, 1048576]
 writes = [0, 20, 40, 60, 80, 100]
 warmup = 5
 duration = 10000
 benchmarks = [#"trees.lockbased.ConcurrencyOptimalBSTv2",
               #"trees.lockbased.ConcurrencyOptimalBSTv3",
-              "trees.lockbased.ConcurrencyOptimalBSTv4",
+#              "trees.lockbased.ConcurrencyOptimalBSTv4",
               "trees.lockbased.ConcurrencyOptimalTreeMap",
               "trees.lockbased.LockBasedFriendlyTreeMap",
-              "trees.lockbased.LockBasedFriendlyTreeMapNoRotation",
+#              "trees.lockbased.LockBasedFriendlyTreeMapNoRotation",
               "trees.lockbased.LogicalOrderingAVL",
-              "trees.lockbased.LockBasedStanfordTreeMap",
-              "trees.lockfree.NonBlockingTorontoBSTMap"]
+#              "trees.lockbased.LockBasedStanfordTreeMap",
+#              "trees.lockfree.NonBlockingTorontoBSTMap"
+              ]
 
 directory = "../output/data-w" + str(warmup) + "-d" + str(duration)
 if not os.path.isdir(directory):
@@ -51,9 +52,13 @@ for key in keys:
                       " cycle list name=color,\n title=Update rate: " + str(write) + "\\%\n]\n")
             for i in range(len(benchmarks)):
                 bench = benchmarks[i]
+                if not os.path.exists(filename(bench, size, write, 1, warmup, duration)):
+                    continue
                 thr1 = mean(read_from_file(filename(bench, size, write, 1, warmup, duration), keys)[key])
                 out.write("\\addplot coordinates {\n")
                 for proc in procs:
+                     if not os.path.exists(filename(bench, size, write, proc, warmup, duration)):
+                         continue
                      thr = mean(read_from_file(filename(bench, size, write, proc, warmup, duration), keys)[key])
                      out.write("\t({}, {:.3})\n".format(proc, int(thr / 1000) / 1000))
                 out.write("};\n")
@@ -69,9 +74,13 @@ for size in sizes:
                   " cycle list name=color,\n smooth,\n title=Update rate: " + str(write) + "\\%\n]\n")
         for i in range(len(benchmarks)):
             bench = benchmarks[i]
+            if not os.path.exists(filename(bench, size, write, 1, warmup, duration)):
+                continue
             out.write("\\addplot coordinates {\n")
             thr1 = mean(read_from_file(filename(bench, size, write, 1, warmup, duration), keys)[key])
             for proc in procs:
+                 if not os.path.exists(filename(bench, size, write, proc, warmup, duration)):
+                     continue
                  thr = mean(read_from_file(filename(bench, size, write, proc, warmup, duration), keys)[key])
                  out.write("\t({}, {:.3})\n".format(proc, thr / thr1))
             out.write("};\n")
